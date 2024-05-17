208cdd682fec349526d1c457acbee5687d06e5ba
castylho <castylho.dev@gmail.com>
2024-05-17 19:14:16 +0100

    added side menu UI with department list

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,24 +1,35 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core';
+import React from "react";
+import { List, ListItem, ListItemText, makeStyles } from "@material-ui/core";
+import { getDepartmentCollection } from "../services/employeeService";
 
 const useStyles = makeStyles({
   sideMenu: {
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'absolute',
-    left: '0px',
-    width: '320px',
-    height: '100%',
-    backgroundColor: '#253053'
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    left: "0px",
+    width: "320px",
+    height: "100%",
+    backgroundColor: "#253053",
   },
 });
 
 export default function SideMenu() {
+  const classes = useStyles();
+  const departments = getDepartmentCollection();
 
-    const classes = useStyles()
-    return (
-        <div className={classes.sideMenu}>
-            
-        </div>
-    )
+  return (
+    <div className={classes.sideMenu}>
+      <List component="nav">
+        {departments.map((department) => (
+          <ListItem key={department.id} button>
+            <ListItemText
+              primary={department.title}
+              style={{ color: "white" }}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 }
