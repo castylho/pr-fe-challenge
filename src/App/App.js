1e22f8458a12d5ef6733afa18a0758e0408e246b
castylho <castylho.dev@gmail.com>
2024-05-17 19:18:34 +0100

    added todo cuz my time was over

--- src/App/App.js
+++ src/App/App.js
@@ -3,31 +3,31 @@ import {
   makeStyles,
   createMuiTheme,
   ThemeProvider,
-} from '@material-ui/core';
-import Header from '../components/Header';
-import SideMenu from '../components/SideMenu';
+} from "@material-ui/core";
+import Header from "../components/Header";
+import SideMenu from "../components/SideMenu";
 
-import Employees from '../pages/Employees/Employees';
-import { seedEmployees } from '../utils/seed.js';
+import Employees from "../pages/Employees/Employees";
+import { seedEmployees } from "../utils/seed.js";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#333996',
-      light: '#3c44b126',
+      main: "#333996",
+      light: "#3c44b126",
     },
     secondary: {
-      main: '#f83245',
-      light: '#f8324526',
+      main: "#f83245",
+      light: "#f8324526",
     },
     background: {
-      default: '#f4f5fd',
+      default: "#f4f5fd",
     },
   },
   overrides: {
     MuiAppBar: {
       root: {
-        transform: 'translateZ(0)',
+        transform: "translateZ(0)",
       },
     },
   },
@@ -40,10 +40,12 @@ const theme = createMuiTheme({
 
 const useStyles = makeStyles({
   appMain: {
-    paddingLeft: '320px',
-    width: '100%',
+    paddingLeft: "320px",
+    width: "100%",
   },
 });
+
+// TODO: add useContext to display users from specified department
 
 function App() {
   const classes = useStyles();
@@ -53,7 +55,7 @@ function App() {
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
-        
+
         <Employees />
       </div>
       <CssBaseline />
